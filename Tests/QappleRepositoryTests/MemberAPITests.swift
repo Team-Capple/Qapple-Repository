2156477f9db54221183d1c08bfb7e993d72de459
thinkyside <eunlyuing@gmail.com>
2025-03-03 18:23:28 +0900

    [#23] 회원가입 인증코드 유닛테스트 추가

--- Tests/QappleRepositoryTests/MemberAPITests.swift
+++ Tests/QappleRepositoryTests/MemberAPITests.swift
@@ -55,41 +55,41 @@ struct MemberAPITests {
     
     @Test("회원가입 인증코드 확인 테스트")
     func checkAuthCode() async throws {
-        // TODO: 메일 인증 화이트리스트 API 관련 이슈 해결 필요
-//        let testEmail = "test\(Date.now.timeIntervalSince1970)@pos.idserve.net"
-//        let testId = "TEST_ID_\(Date.now.description)"
-//        let testDeviceToken = "TEST_DEVICE_TOKEN"
-//        
-//        let localSignUp = try await MemberAPI.localSignIn(
-//            testId: testId,
-//            deviceToken: testDeviceToken,
-//            server: .test
-//        )
-//        let signUp = try await MemberAPI.signUp(
-//            signUpToken: localSignUp.refreshToken,
-//            email: testEmail,
-//            nickname: "nickname",
-//            deviceToken: testDeviceToken,
-//            server: .test
-//        )
-//        let _ = try await MemberAPI.roleChange(
-//            role: .admin,
-//            server: .test,
-//            accessToken: signUp.accessToken ?? ""
-//        )
-//        let _ = try await MemberAPI.emailWhitelist(
-//            email: testEmail,
-//            durationMinutes: 1,
-//            server: .test,
-//            accessToken: signUp.accessToken ?? ""
-//        )
-//        let response = try await MemberAPI.checkAuthCode(
-//            signUpToken: signUp.refreshToken ?? "",
-//            email: testEmail,
-//            certCode: "CERT7",
-//            server: .test
-//        )
-//        dump(response)
+        let now = Date.now
+        let testEmail = "test\(now.timeIntervalSince1970)date25@pos.idserve.net"
+        
+        let accessToken = try await TestHelper.accessToken()
+        let _ = try await MemberAPI.roleChange(
+            role: .admin,
+            server: .test,
+            accessToken: accessToken
+        )
+        let _ = try await MemberAPI.emailWhitelist(
+            email: testEmail,
+            durationMinutes: 10,
+            server: .test,
+            accessToken: accessToken
+        )
+        let localSignUp = try await MemberAPI.localSignIn(
+            testId: "TEST_ID_\(now.description)",
+            deviceToken: "TEST_DEVICE_TOKEN_\(now.description)",
+            server: .test
+        )
+        let _ = try await MemberAPI.sendCertificationEmail(
+            signUpToken: localSignUp.refreshToken,
+            email: testEmail,
+            server: .test
+        )
+        guard let certCode = ProcessInfo.processInfo.environment["CERT_CODE"] else {
+            throw RepositoryError.invalidSecretKey("CERT_CODE")
+        }
+        let response = try await MemberAPI.checkAuthCode(
+            signUpToken: localSignUp.refreshToken,
+            email: testEmail,
+            certCode: certCode,
+            server: .test
+        )
+        dump(response)
     }
     
     @Test("닉네임 중복 확인 테스트")
