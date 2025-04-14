2c818430096fe5661e406e8a262c48bff4b6cbe8
mooninbeom <dlsqja567@naver.com>
2025-04-14 21:10:37 +0900

    [#33] DTO 생성

---
+++ Sources/QappleRepository/DTO/AnswerComment/AnswerCommentList.swift
@@ -0,0 +1,21 @@
+//
+//  AnswerCommentList.swift
+//  QappleRepository
+//
+//  Created by 문인범 on 4/14/25.
+//
+
+import Foundation
+
+
+public struct AnswerCommentList: Decodable, Sendable {
+    public let answerCommentInfos: [Content]
+    
+    public struct Content: Decodable, Sendable {
+        public let answerCommentId: Int
+        public let writerId: Int
+        public let content: String
+        public let heartCount: Int
+        public let createdAt: String
+    }
+}
